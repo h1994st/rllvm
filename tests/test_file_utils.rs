a9e9e0c174b4b346b66ba5e4de9be54a9c765ecc
h1994st <h1994st@gmail.com>
2023-06-20 00:05:38 -0700

    Update unit tests

--- tests/test_file_utils.rs
+++ tests/test_file_utils.rs
@@ -8,13 +8,13 @@ mod tests {
     use rllvm::utils::*;
 
     #[test]
-    fn test_path_injection() {
+    fn test_path_injection_and_extraction() {
         let bitcode_filepath = Path::new("tests/data/hello.bc");
         let object_filepath = Path::new("tests/data/hello.o");
 
-        // let output_object_filepath = Path::new("/tmp/hello.new.o");
-        let output_object_filepath = Path::new("foo.o");
+        let output_object_filepath = Path::new("/tmp/hello.new.o");
 
+        // Embed bitcode filepath
         let ret = embed_bitcode_filepath_to_object_file(
             bitcode_filepath,
             object_filepath,
@@ -22,16 +22,8 @@ mod tests {
         );
         assert!(ret.is_ok());
 
-        // Clean
-        fs::remove_file(output_object_filepath).expect("Failed to delete the output object file");
-    }
-
-    #[test]
-    fn test_path_extraction() {
-        let bitcode_filepath = Path::new("tests/data/hello.bc");
-        let object_filepath = Path::new("tests/data/hello.rllvm.o");
-
-        let embedded_filepaths = extract_bitcode_filepaths_from_object_file(object_filepath)
+        // Extract embedded filepaths
+        let embedded_filepaths = extract_bitcode_filepaths_from_object_file(output_object_filepath)
             .expect("Failed to extract embedded filepaths");
         assert!(!embedded_filepaths.is_empty());
 
@@ -41,6 +33,9 @@ mod tests {
             .expect("Failed to obtain the absolute filepath");
         println!("{:?}", embedded_filepath);
         assert_eq!(embedded_filepath, expected_filepath);
+
+        // Clean
+        fs::remove_file(output_object_filepath).expect("Failed to delete the output object file");
     }
 
     #[test]
