fb223d83256a53fa97f47fcf7b761db768ec12ee
Duna Feline <duna.feline@gmail.com>
2026-02-16 01:23:59 -0800

    refactor: adopt thiserror for Error enum (#5)

--- src/error.rs
+++ src/error.rs
@@ -2,43 +2,35 @@
 
 use std::{str::Utf8Error, string::FromUtf8Error};
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// Invalid arguments
+    #[error("Invalid arguments: {0}")]
     InvalidArguments(String),
     /// Io error occurred
-    Io(std::io::Error),
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
     /// Command execution failure
+    #[error("Execution failure: {0}")]
     ExecutionFailure(String),
-    /// Object file error
-    ObjectReadError(object::read::Error),
-    ObjectWriteError(object::write::Error),
+    /// Object file read error
+    #[error("Object read error: {0}")]
+    ObjectReadError(#[from] object::read::Error),
+    /// Object file write error
+    #[error("Object write error: {0}")]
+    ObjectWriteError(#[from] object::write::Error),
     /// String error
+    #[error("String error: {0}")]
     StringError(String),
     /// Logger error
+    #[error("Logger error: {0}")]
     LoggerError(String),
     /// Missing file
+    #[error("Missing file: {0}")]
     MissingFile(String),
     /// Something else happened
+    #[error("Unknown error: {0}")]
     Unknown(String),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(value: std::io::Error) -> Self {
-        Self::Io(value)
-    }
-}
-
-impl From<object::read::Error> for Error {
-    fn from(value: object::read::Error) -> Self {
-        Self::ObjectReadError(value)
-    }
-}
-
-impl From<object::write::Error> for Error {
-    fn from(value: object::write::Error) -> Self {
-        Self::ObjectWriteError(value)
-    }
 }
 
 impl From<Utf8Error> for Error {
